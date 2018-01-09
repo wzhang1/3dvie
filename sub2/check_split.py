dee6de0c1cc75c7dfbe33c4bb84389d2275356f6
wzhang1 <zhangvszhang@gmail.com>
2018-01-09 08:52:34 -0800

    check_split_bug

--- sub2/check_split.py
+++ sub2/check_split.py
@@ -16,13 +16,20 @@ wrong = 0
 # be careful, some of the model are traine with background classes
 
 
-with open("./12new440x440-ep15.csv") as fp:
+with open("../sub1/new500x500-ep11.csv") as fp:
 
+    line = fp.readline();
     line = fp.readline();
     while line:
         l_array = line.rstrip().split(",")
-        print(labels[l_array[0]])
-        if (2 == labels[l_array[0]] or 1 == labels[l_array[0]]):
+        #print(labels[l_array[0]])
+        #print(l_array)
+        predict1 = int(l_array[1][0])
+        #print(predict)
+        predict2 = int(l_array[1][2])
+        #print(predict2)
+        
+        if (predict2 == labels[l_array[0]] or predict1 == labels[l_array[0]]):
             correct = correct + 1
         else:
             wrong = wrong + 1
