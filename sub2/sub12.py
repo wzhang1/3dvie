0b6cb5ab2127a617e52d696bd88e47246149ae84
wzhang1 <zhangvszhang@gmail.com>
2018-01-10 18:20:27 -0800

    finallysub2bug

--- sub2/sub12.py
+++ sub2/sub12.py
@@ -59,8 +59,11 @@ def predict(prefix, epoch, gpu_id, test_lst_file, img_sz, crop_sz, batch_sz=64):
     with open(test_lst_file) as fp:
         line = fp.readline();
         while line:
-            l_array = line.rstrip().split(",")
-            imgs.append(l_array[0])
+            #l_array = line.rstrip().split(",")
+            l_array = line.rstrip().split("\t")
+            print(l_array)
+            
+            imgs.append(l_array[2])
             line = fp.readline();
 
     classes = [0]*len(imgs)
@@ -76,11 +79,13 @@ def predict(prefix, epoch, gpu_id, test_lst_file, img_sz, crop_sz, batch_sz=64):
     im_probs = []
     result=[]
 
-    szx = 337
-    szy = 600
+    #szx = 337
+    #szy = 600
+    szx = 600
+    szy = 337
     #batch_sz = 64
     #input_blob = np.zeros((batch_sz,3,szx,crop_szy))
-    input_blob = np.zeros((batch_sz,3, szx, szy))
+    input_blob = np.zeros((batch_sz,3, 600, 337))
     idx = 0
     num_batches = int(len(imgs) / batch_sz)
 
@@ -98,18 +103,12 @@ def predict(prefix, epoch, gpu_id, test_lst_file, img_sz, crop_sz, batch_sz=64):
             img_full_name = test_folder  + img_name
             #print(img_full_name)
             img = cv2.cvtColor(cv2.imread(img_full_name), cv2.COLOR_BGR2RGB)
+            img= cv2.resize(img, (337, 600))
             img = np.float32(img)
-            img = mx.nd.array(img)
-            #(1080, 1920,3)
-            #print(img.shape)
-            img = mx.img.resize_short(img, 337 * 6 /5)
-            #img = center_crop(src, (600, 337))
-            img = mx.img.center_crop(img, (337, 600))
-
-
             img = np.swapaxes(img, 0, 2)
             img = np.swapaxes(img, 1, 2)  # change to r,g,b order
-            
+            #print(img.shape)
+            #print(input_blob.shape)
             input_blob[idx,:,:,:] = img
             idx += 1
 	    #print(idx)
@@ -123,12 +122,10 @@ def predict(prefix, epoch, gpu_id, test_lst_file, img_sz, crop_sz, batch_sz=64):
         exe.forward(is_train=False)
         net_out = exe.outputs[0].asnumpy()
 
-        input_blob = np.zeros((batch_sz,3,szx, szy))
-
         for bz in range(batch_sz):
 	    probs = net_out[bz,:]
     	    score = np.squeeze(probs)
-            #print(probs)
+            print(probs)
             im_probs.append(probs)
 
             sort_index = np.argsort(score)[::-1]
@@ -146,11 +143,12 @@ def predict(prefix, epoch, gpu_id, test_lst_file, img_sz, crop_sz, batch_sz=64):
         cnt += 1
         img_full_name = test_folder + img_name
         img = cv2.cvtColor(cv2.imread(img_full_name), cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (337, 600))
         img = np.float32(img)
         img = mx.nd.array(img)
                     
-        img = mx.img.resize_short(img, 337 * 6 /5)
-        img = mx.image.center_crop(img, (600, 337))
+        #img = mx.img.resize_short(img, 337 * 6 /5)
+        #img = mx.image.center_crop(img, (600, 337))
 
         img = np.swapaxes(img, 0, 2)
         img = np.swapaxes(img, 1, 2)  # change to r,g,b order
@@ -162,7 +160,7 @@ def predict(prefix, epoch, gpu_id, test_lst_file, img_sz, crop_sz, batch_sz=64):
         exe = sym.bind(ctx, arg_params ,args_grad=None, grad_req="null", aux_states=aux_params)
         exe.forward(is_train=False)
         probs = exe.outputs[0].asnumpy()
-        #print(probs)
+        print(probs)
         im_probs.append(probs)
         score = np.squeeze(probs.mean(axis=0))
 
@@ -183,7 +181,7 @@ def predict(prefix, epoch, gpu_id, test_lst_file, img_sz, crop_sz, batch_sz=64):
 
 #im_idxs12, preds12, im_probs12 = predict("../model_12/3dvie-resnet-152", 25, 1, "12new440x440-ep15.csv", 440, 550, 4)
 #im_idxs12, preds12, im_probs12 = predict("../model_level3/3dvie-resnet-18_12", 10, 1, "../data/image_dev12.csv", 440, 440, 4)
-im_idxs12, preds12, im_probs12 = predict("../model_12/3dvie-resnet-152", 3, 1, "../data/image_dev12.csv", 337, 600, 4)
+im_idxs12, preds12, im_probs12 = predict("../model_12/3dvie-resnet-152", 3, 0, "../data/image_dev12.lst", 337, 600, 64)
 print(im_idxs12)
 print(len(preds12))
 print(len(im_probs12))
